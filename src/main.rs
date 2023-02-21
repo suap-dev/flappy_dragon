fc779cc8f455ddcc1db94745fae111f7923da9b4
suap-dev <suap.dev@gmail.com>
2023-02-21 22:46:29 +0100

    init terminal

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
-fn main() {
-    println!("Hello, world!");
+#![warn(
+    clippy::all,
+    clippy::pedantic,
+    clippy::nursery,
+)]
+
+use bracket_lib::prelude::*;
+
+fn main() -> BError {
+    let context = BTermBuilder::simple80x50()
+        .with_title("Flappy Dragon")
+        .build()?;
+
+    main_loop(context, State {})
+}
+
+struct State {}
+impl GameState for State {
+    fn tick(&mut self, ctx: &mut BTerm) {
+        ctx.cls();
+        ctx.print(1, 1, "Hello, dear Terminal!");
+    }
 }
