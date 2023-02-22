34db6fb4c47585e41d044d0f5b12130a5529d0c8
suap-dev <suap.dev@gmail.com>
2023-02-22 05:42:40 +0100

    add diferent game states/modes

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,56 @@
-#![warn(
-    clippy::all,
-    clippy::pedantic,
-    clippy::nursery,
-)]
+#![warn(clippy::all, clippy::pedantic, clippy::nursery)]
 
 use bracket_lib::prelude::*;
+
+#[derive(Debug)]
+enum GameMode {
+    Menu,
+    Playing,
+    End,
+}
 
 fn main() -> BError {
     let context = BTermBuilder::simple80x50()
         .with_title("Flappy Dragon")
         .build()?;
 
-    main_loop(context, State {})
+    main_loop(context, State::new())
 }
 
-struct State {}
+struct State {
+    mode: GameMode,
+}
+impl State {
+    const fn new() -> Self {
+        Self {
+            mode: GameMode::Menu,
+        }
+    }
+
+    fn main_menu(&mut self, ctx: &mut BTerm) {
+        println!("{:?}", self.mode);
+        // TODO
+
+        self.mode = GameMode::Playing;
+    }
+    fn dead(&self, ctx: &mut BTerm) {
+        println!("{:?}", self.mode);
+        // TODO
+        ctx.quitting = true;
+    }
+    fn play(&mut self, ctx: &mut BTerm) {
+        println!("{:?}", self.mode);
+        // TODO
+
+        self.mode = GameMode::End;
+    }
+}
 impl GameState for State {
     fn tick(&mut self, ctx: &mut BTerm) {
-        ctx.cls();
-        ctx.print(1, 1, "Hello, dear Terminal!");
+        match self.mode {
+            GameMode::Menu => self.main_menu(ctx),
+            GameMode::Playing => self.play(ctx),
+            GameMode::End => self.dead(ctx),
+        }
     }
 }
