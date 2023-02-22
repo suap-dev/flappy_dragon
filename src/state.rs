961ddc807fa6b4214d65c6539f6ab278823576e1
suap-dev <suap.dev@gmail.com>
2023-02-22 23:01:46 +0100

    let it fly

--- src/state.rs
+++ src/state.rs
@@ -1,3 +1,4 @@
+use crate::player::Player;
 use bracket_lib::prelude::*;
 
 #[derive(Debug)]
@@ -7,33 +8,82 @@ enum GameMode {
     End,
 }
 
+const SCREEN_WIDTH: i32 = 80;
+const SCREEN_HEIGHT: i32 = 50;
+const FRAME_DURATION: f32 = 75.0; // ms; meaning 13.(3) fps
+
 pub struct State {
+    player: Player,
+    frame_time: f32,
     mode: GameMode,
 }
 impl State {
-    #[must_use]
     pub const fn new() -> Self {
         Self {
+            player: Player::new(5, 25),
+            frame_time: 0.0,
             mode: GameMode::Menu,
         }
     }
 
     fn main_menu(&mut self, ctx: &mut BTerm) {
         println!("{:?}", self.mode);
-        // TODO
 
-        self.mode = GameMode::Playing;
+        ctx.cls();
+        ctx.print_centered(5, "Welcome to Flappy Dragon!");
+        ctx.print_centered(8, "(P) Play Game");
+        ctx.print_centered(9, "(Q) Quit GAme");
+
+        if let Some(key) = ctx.key {
+            match key {
+                VirtualKeyCode::P => self.restart(),
+                VirtualKeyCode::Q => ctx.quitting = true,
+                _ => {}
+            }
+        }
     }
-    fn dead(&self, ctx: &mut BTerm) {
+    fn dead(&mut self, ctx: &mut BTerm) {
         println!("{:?}", self.mode);
-        // TODO
-        ctx.quitting = true;
+
+        ctx.cls();
+        ctx.print_centered(5, "You died.");
+        ctx.print_centered(8, "(P) Play Again");
+        ctx.print_centered(9, "(Q) Quit GAme");
+
+        if let Some(key) = ctx.key {
+            match key {
+                VirtualKeyCode::P => self.restart(),
+                VirtualKeyCode::Q => ctx.quitting = true,
+                _ => {}
+            }
+        }
     }
     fn play(&mut self, ctx: &mut BTerm) {
         println!("{:?}", self.mode);
-        // TODO
 
-        self.mode = GameMode::End;
+        ctx.cls_bg(NAVY);
+        self.frame_time += ctx.frame_time_ms;
+        if self.frame_time > FRAME_DURATION {
+            self.frame_time = 0.0;
+        }
+
+        self.player.gravity_and_move();
+
+        if let Some(VirtualKeyCode::Space) = ctx.key {
+            self.player.flap();
+        }
+
+        self.player.render(ctx);
+        ctx.print(0, 0, "Press SPACE to flap.");
+
+        if self.player.y > SCREEN_HEIGHT {
+            self.mode = GameMode::End;
+        }
+    }
+    fn restart(&mut self) {
+        self.player = Player::new(5, 25);
+        self.frame_time = 0.0;
+        self.mode = GameMode::Playing;
     }
 }
 impl GameState for State {
