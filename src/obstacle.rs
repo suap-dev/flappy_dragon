f098315be47b3a583f10f8f8bb276e06ec8e837a
suap-dev <suap.dev@gmail.com>
2023-02-23 03:44:43 +0100

    obstacle struct

---
+++ src/obstacle.rs
@@ -0,0 +1,49 @@
+use bracket_lib::prelude::*;
+
+use crate::{consts::SCREEN_HEIGHT, player::Player};
+
+pub struct Obstacle {
+    x: i32,
+    gap_y: i32,
+    gap_size: i32,
+    /*
+        obstacle looks like this:            
+                                    |
+                                    |
+                                    |
+                        gap_y _             } gap size
+                                            } --------
+                                    |
+                                    |
+                                    |    
+     */
+}
+impl Obstacle {
+    pub fn new(x: i32, score: i32) -> Self {
+        let mut random = RandomNumberGenerator::new();
+        Self {
+            x,
+            gap_y: random.range(10, 40),
+            gap_size: i32::max(2, 20 - score),
+        }
+    }
+    pub fn render(&mut self, ctx: &mut BTerm, player_x: i32) {
+        let screen_x = self.x - player_x;
+        let half_size = self.gap_size / 2;
+
+        for y in 0..self.gap_y - half_size {
+            ctx.set(screen_x, y, RED, BLACK, to_cp437('|'));
+        }
+
+        for y in self.gap_y + half_size..SCREEN_HEIGHT {
+            ctx.set(screen_x, y, RED, BLACK, to_cp437('|'));
+        }
+    }
+    pub fn hit_obstacle(&mut self, player: &Player) -> bool {
+        let gap_top = self.gap_y - self.gap_size / 2;
+        let gap_bottom = self.gap_y + self.gap_size / 2;
+
+        // if player is at the same X and NOT inside the gap:
+        player.x == self.x && !(player.y < gap_top && player.y > gap_bottom)
+    }
+}
